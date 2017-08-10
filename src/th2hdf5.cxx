7321b68ecab3c4504a600bbfb5867d3054c92267
Dan Guest <dguest@cern.ch>
2017-08-10 17:12:14 +0200

    Add usage

--- src/th2hdf5.cxx
+++ src/th2hdf5.cxx
@@ -4,8 +4,15 @@
 #include "H5Cpp.h"
 #include "TFile.h"
 
+void usage(const char* prog) {
+  printf("usage: %s <root-file> <output-file-name>\n", prog);
+}
+
 int main(int argc, char* argv[]) {
-  if (argc <= 2) return 1;
+  if (argc != 3) {
+    usage(argv[0]);
+    return 1;
+  }
   unshittify();
   TFile root_file(argv[1], "read");
   H5::H5File hdf_file(argv[2], H5F_ACC_TRUNC);
